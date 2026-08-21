b8835a9fb4efb7bc441372b3d7b7f0d075fa72f6
ramjason05 <ramjason05@gmail.com>
2026-08-20 21:43:12 -0700

    added description to embeddings script

--- gen_embeddings.py
+++ gen_embeddings.py
@@ -1,8 +1,17 @@
 import os
+from pathlib import Path
 from supabase import create_client
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
-load_dotenv()
+"""
+Embedding all the information into one text blob that says everything about the class/professor
+
+That text blob turns into embeddings and that is what is compared
+
+"""
+
+ROOT = Path(__file__).resolve().parent
+load_dotenv(ROOT / ".env")
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
