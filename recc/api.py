5b211c54f5ffe9938841f8b609941465bb1a33f6
ramjason05 <ramjason05@gmail.com>
2026-08-22 09:44:50 -0700

    merge conflicts

--- recc/api.py
+++ recc/api.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # match your actual React dev server port
+    allow_origins=["http://localhost:5173"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
